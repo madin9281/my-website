3c0f011e32fd504fb18af01e091352a332f24082
madin9281 <madin9281@gmail.com>
2026-04-25 12:45:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,14 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        return f"User {username} registered successfully ✅"
+
     return render_template('register.html')
 
 if __name__ == "__main__":
